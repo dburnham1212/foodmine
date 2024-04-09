f7ab1e845fc4233e02551eba58116f92408d86b0
Dylan Burnham <dburnham1212@gmail.com>
2024-04-09 18:05:28 -0400

    Adding search to front page

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/Food';
 import { FoodService } from '../../../services/food.service';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { StarRatingComponent } from '../../partials/star-rating/star-rating.component';
+import { SearchComponent } from '../../partials/search/search.component';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +12,8 @@ import { StarRatingComponent } from '../../partials/star-rating/star-rating.comp
   imports: [
     CommonModule,
     RouterModule,
-    StarRatingComponent
+    StarRatingComponent,
+    SearchComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
@@ -19,9 +21,14 @@ import { StarRatingComponent } from '../../partials/star-rating/star-rating.comp
 export class HomeComponent {
   foods: Food[] = [];
 
-  constructor(private foodService: FoodService) {
-    this.foods = foodService.getAll();
-    console.log(this.foods)
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if(params['searchTerm']) {
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params["searchTerm"]);
+      } else {
+        this.foods = foodService.getAll();
+      }
+    })
   }
 
 
